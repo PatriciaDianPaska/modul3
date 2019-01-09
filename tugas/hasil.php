3c9c9e5fb7fbcce2b205ee6b4d2268e17a6d4662
PatriciaDianPaska <patricia_dian_26rpl@student.smktelkom-mlg.sch.id>
2019-01-09 12:50:11 +0700

    first commit

---
+++ tugas/hasil.php
@@ -0,0 +1,9 @@
+<?php
+$email = $_POST ['email'];
+$pass = $_POST ['pass'];
+ if ($email == "firdausa@gmail.com" &&  $pass == "123") {
+   echo "Login Berhasil";
+ } else {
+   echo "Login Gagal";
+ }
+ ?>
